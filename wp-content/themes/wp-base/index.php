3df512dc2ceb4aa0e05afc04e7951e56c78bb6ff
Elliot Pettingale <elliot@smartercoding.co.uk>
2021-10-09 16:31:05 +0100

    refactor controllers

--- wp-content/themes/wp-base/index.php
+++ wp-content/themes/wp-base/index.php
@@ -4,6 +4,10 @@ use BoxyBird\Inertia\Inertia;
 use EliPett\Transformation\Services\Transform;
 use SmarterCoding\WpBase\Transformers\PostTransformer;
 
-return Inertia::render('Index', [
+global $posts;
+
+Inertia::render('Index', [
     'posts' => Transform::all($posts, PostTransformer::class)
 ]);
+
+return;
--- wp-content/themes/wp-base/page.php
+++ wp-content/themes/wp-base/page.php
@@ -4,6 +4,10 @@ use BoxyBird\Inertia\Inertia;
 use EliPett\Transformation\Services\Transform;
 use SmarterCoding\WpBase\Transformers\PostTransformer;
 
-return Inertia::render('Page', [
+global $post;
+
+Inertia::render('Page', [
     'page' => Transform::one($post, PostTransformer::class)
 ]);
+
+return;
