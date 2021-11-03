601aa67181e364e932509d7632210ee82bd925fc
Elliot Pettingale <elliot@smartercoding.co.uk>
2021-11-03 10:13:41 +0000

    remove test code

--- wp-content/themes/wp-base/functions.php
+++ wp-content/themes/wp-base/functions.php
@@ -2,29 +2,7 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-use EliPett\Transformation\Services\Transform;
-use SmarterCoding\WpBase\Transformers\PostTransformer;
 use SmarterCoding\WpBase\Providers\ThemeServiceProvider;
-use SmarterCoding\WpPlus\Helpers\Router;
-use SmarterCoding\WpPlus\Structs\Request;
 
 $themeServiceProvider = new ThemeServiceProvider();
 $themeServiceProvider->boot();
-
-Router::get('/html/[i:id]', function(Request $request) {
-    $post = get_post($request->route('id'));
-
-    return response()
-        ->render('Page', [
-            'page' => Transform::one($post, PostTransformer::class)
-        ]);
-});
-
-Router::get('/json/[i:id]', function(Request $request) {
-    $post = get_post($request->route('id'));
-
-    $item = Transform::one($post, PostTransformer::class);
-
-    return response()
-        ->json($item);
-});
